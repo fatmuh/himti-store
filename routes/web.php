230a9ad3eb546ea2f92f3cd02f54bbed0c2ace27
Mohammad Entol Rizky <mohammad.erm08@gmail.com>
2024-02-17 04:10:23 +0700

    Delete

--- routes/web.php
+++ routes/web.php
@@ -30,12 +30,6 @@ Route::controller(LandingController::class)->name('landing.')->group( function()
     Route::get('/ticket','ticket')->name('ticket');
     Route::get('/ticket/{uniq}','ticketDetail')->name('ticket.detail');
 
-    Route::get('/OTP-MIGRATION-DO-NOT-USE-THIS',function(){
-        \Artisan::call('migrate:fresh');
-        \Artisan::call('storage:link');
-
-        dd('Done do storage:link && migrate:fresh');
-    });
 });
 
 Auth::routes();
